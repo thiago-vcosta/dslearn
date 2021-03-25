6151472db6360bf74d72897c0d551091d75f91bf
thiago-vcosta <thiago.vcosta@uol.com.br>
2021-03-24 21:55:17 -0300

    Notification unreadonly

--- backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
+++ backend/src/main/java/com/devsuperior/dslearn/repositories/NotificationRepository.java
@@ -1,9 +1,20 @@
 package com.devsuperior.dslearn.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.devsuperior.dslearn.entities.Notification;
+import com.devsuperior.dslearn.entities.User;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long>{
 
+	@Query("SELECT obj FROM Notification obj WHERE "
+			+ "(obj.user = :user) AND "
+			+ "(:unreadOnly = false OR obj.read = false) "
+			+ "ORDER BY obj.moment DESC")
+	Page<Notification> find(User user, boolean unreadOnly, Pageable pageable);
 }
